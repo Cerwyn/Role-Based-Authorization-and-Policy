6216462ecdd5a45b65b6f285b0c16cdc67e69a21
cerwyn <cerwyneliata.c@gmail.com>
2020-05-04 19:53:36 +0700

    Update welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,14 +85,9 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/create/thread">Create a Thread</a>
+                    <a href="/update/thread">Update a Thread</a>
+                    <a href="/delete/thread">Delete a Thread</a>
                 </div>
             </div>
         </div>
